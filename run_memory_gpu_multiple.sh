ce179534b242d0ffd113eaa2860a9639e591a7e5
Sara980710 <sara@idala.eu>
2022-04-29 13:36:44 +0200

    trying docker for memory measures

--- run_memory_gpu_multiple.sh
+++ run_memory_gpu_multiple.sh
@@ -2,51 +2,57 @@
 
 # (0:f32, 1:f16, 2:int8)
 declare -a precisions=( "0" "1" ) #( "0" "1" "2" ) 
-declare -a types=( "yolo_models" "class_models" )
-declare -a sizes=( "192" "384" "768" "1152" "1536" )#( "32" "192" "384" "768" "1152" "1536" )
+declare -a types=( "yolo_models" "class_models" ) #( "yolo_models" "class_models" )
+declare -a sizes=( "192" "384" "768" "1152" "1536" ) # ( "32" "192" "384" "768" "1152" "1536" )
+declare -a devices=( "0" "1" ) #( "1" "0" ) # (0:cpu or 1:gpu)
+declare -a batch_sizes=( "1" ) #( "1" "5" "10" "50" "100")
 
-iterations="3"
-device="0" # (0:"cpu" or 1:"gpu")
 threads="1"
+verbose="0" # (0:false, 1:true)
 
 image="big.jpg"
-pathTFLite="/home/ebara/Documents/master_thesis/TFLite"
-#pathTFLite="/home/sara/Documents/Master-thesis/TFLite"
+pathTFLite="/home/ebara/Documents/master_thesis/TFLite" # AI Sweden computer
+#pathTFLite="/home/sara/Documents/Master-thesis/TFLite" # Saras computer
 #pathTFLite="/home/spacecloud/ebara/ml_performancetests" # ix5
 
-echo "Info: precisions:$precisions, types:$types, sizes:$sizes, iterations:$iterations, device:$device, path_models:$pathTFLite/models, image: $image, nr threads: $threads"
-echo ""
+echo "Info: precisions:$precisions, types:$types, sizes:$sizes, iterations:$iterations, device:$device, path_models:$pathTFLite/models, image: $image, nr threads: $threads, verbose: $verbose"
 
-for type in "${types[@]}"; do
-    for precision in "${precisions[@]}"; do
-        for size in "${sizes[@]}"; do  
-            if [ "$precision" = "0" ]; then
-                model="${size}-fp32.tflite"
-            elif [ "$precision" = "1" ]; then
-                model="${size}-fp16.tflite"
-            elif [ "$precision" = "2" ]; then
-                model="${size}-int8.tflite"
-            fi
-            echo "--------------------------------------------------------------"
-            echo "Type: $type, Precision: $precision, Size: $size, Model: $model"
-            echo ""
-            echo "Starting measure-session"
-            tmux new-session -d -s measure-session "while true; 
-                do nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv >> gpu_utilization_test/$type$model.log; sleep 0.1; 
-                done" &
+for device in ${devices[@]}; do
+    for type in "${types[@]}"; do
+        for precision in "${precisions[@]}"; do
+            for size in "${sizes[@]}"; do  
+                for bs in "${batch_sizes[@]}"; do 
 
-            sleep 1;
-            echo "Starting program"
-            
-            cd build; 
-            ./TFLiteMemory $pathTFLite/models/$type/$model $precision $pathTFLite/data/$image $threads $device;
-            cd ..;
-            
-            sleep 1;
-            echo "Killing measure-session"
-            tmux kill-session -t measure-session;
-            echo ""
-            sleep 10;
+                if [ "$precision" = "0" ]; then
+                    model="${size}-fp32.tflite"
+                elif [ "$precision" = "1" ]; then
+                    model="${size}-fp16.tflite"
+                elif [ "$precision" = "2" ]; then
+                    model="${size}-int8.tflite"
+                fi
+                echo "--------------------------------------------------------------"
+                echo "Type: $type, Precision: $precision, Size: $size, Model: $model"
+                echo ""
+                echo "Starting measure-session"
+                tmux new-session -d -s measure-session "while true; 
+                    do nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv >> gpu_utilization_test/$type$model.log; sleep 0.1; 
+                    done" &
+
+                sleep 1;
+                echo "Starting program"
+                
+                cd build; 
+                ./TFLiteMemory $pathTFLite/models/$type/$model $precision $pathTFLite/data/$image $threads $device $verbose;
+                cd ..;
+                
+                sleep 1;
+                echo "Killing measure-session"
+                tmux kill-session -t measure-session;
+                echo ""
+                sleep 10;
+
+                done
+            done
         done
     done
 done
--- src/tflite_mem.cpp
+++ src/tflite_mem.cpp
@@ -3,8 +3,9 @@
 
 
 int main(int argc, char **argv) {
-  if (argc != 6) {
-    throw std::invalid_argument("Required arguments: \n            -path to TFLite model file \n            -desired precision (0:f32, 1:f16, 2:int8)\n            -path to image input\n            -Number of threads\n            -device(gpu or cpu)");
+  if (argc != 7) {
+    std::cout<<argc<<std::endl;
+    throw std::invalid_argument("Required arguments: \n            -path to TFLite model file \n            -desired precision (0:f32, 1:f16, 2:int8)\n            -path to image input\n            -Number of threads\n            -device (0:cpu or 1:gpu)\n            -verbose (0:false, 1:true)");
   }
   const char *modelFileName = argv[1];
   const int desiredPrecision = std::stoi(argv[2]);
@@ -15,8 +16,12 @@ int main(int argc, char **argv) {
   if (std::stoi(argv[5]) == 1) {
     gpu = true;
   }
-  
+
   bool verbose = false;
+  if (std::stoi(argv[6]) == 1) {
+    verbose = true;
+  }
+  
   bool normalize = true;
 
   Detector detector(modelFileName, gpu, threads, verbose);
